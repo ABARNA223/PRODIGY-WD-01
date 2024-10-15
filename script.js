75ca019f70706561689ceb1c7e428a168150ef65
ABARNA223 <abarnasivakumar223@gmail.com>
2024-10-15 17:21:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetSection = document.querySelector(this.getAttribute('href'));
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
+  
+  
